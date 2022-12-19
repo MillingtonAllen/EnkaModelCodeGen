2a29f2faf9ec6afb0beba7de87c3cbd19cfd4d4e
MillingtonAllen <pyro226@gmail.com>
2022-12-18 20:37:35 -0600

    Added imports for Map and List to java files

--- writer/writer.py
+++ writer/writer.py
@@ -1,3 +1,6 @@
+from builder.TypedBuilderFactory import is_list, is_map
+
+
 class Writer:
     def __init__(self, java_class):
         self.java_class = java_class
@@ -8,6 +11,24 @@ class Writer:
         output += '''\n'''
         output += f'''import lombok.Data;\n'''
         output += '''\n'''
+
+        contains_map = False
+        contains_list = False
+        for member in self.java_class.members:
+            if member.variable_type.startswith("Map"):
+                contains_map = True
+            if member.variable_type.startswith("List"):
+                contains_list = True
+
+        if contains_list:
+            output += f'''import java.util.List;\n'''
+
+        if contains_map:
+            output += f'''import java.util.Map;\n'''
+
+        if contains_map or contains_list:
+            output += '''\n'''
+
         output += '''@Data\n'''
         output += f'''public class {self.java_class.class_name} {{\n'''
 
